9953990197c1a655e96ed8e72a01b27aa056c5c6
Daria Stikheeva <dstikheeva@yandex.ru>
2024-12-17 12:45:48 +0300

    add task 2.12.1

---
+++ Practice/2 Data Structures/2.12 Arrays/Task1.py
@@ -0,0 +1,35 @@
+class Array:
+    """
+    Линейный ститический массив.
+    """
+
+    def __init__(self, size):
+        # Данные массива, изначально массив пустой и все его элементы заполнены None.
+        # То есть сразу выделяем массив фиксированного объема.
+        self.data = [None] * size
+
+        # Длина заполнненого массива.
+        # По умолчанию 0, так как массив пустой.
+        self.length = 0
+
+        # Полный размер массива.
+        self.size = size
+
+    def append(self, value):
+        """
+        Добавление нового элемента в конец линейного массива.
+        Время работы O(1).
+        """
+        # Введите ваш код тут
+        if self.length < self.size:
+            self.data[self.length] = value
+            self.length += 1
+        else:
+            raise OverflowError
+
+    def __str__(self):
+        """
+        Возвращает все элементы массива в виде строки.
+        """
+        return "[" + ", ".join(map(str, self.data[:self.length])) + "]"
+    
